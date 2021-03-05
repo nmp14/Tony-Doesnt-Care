8cc6a73239b973f64875b3add3d0ea2ce2b0d549
Nathan Peek <nmp14fsu@gmail.com>
2021-03-05 18:39:17 -0500

    Create server with express

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const chemicals = [];
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Paths
+app.use("/assets", express.static('assets'));
+
+// HTML paths
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/storage', (req, res) => res.sendFile(path.join(__dirname, 'storage.html')));
+app.get("/addForm", (req, res) => res.sendFile(path.join(__dirname, 'addForm.html')));
+// API paths
+app.get("/api/chemicals", (req, res) => res.json(chemicals));
+
+// Listen on port
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
