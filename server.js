441ab50d28810fcb56765102159235105786b399
Nathan Peek <nmp14fsu@gmail.com>
2021-03-05 18:58:48 -0500

    Add post path for adding chemicals

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const chemicals = [];
+let count = 0;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -19,6 +20,18 @@ app.get('/storage', (req, res) => res.sendFile(path.join(__dirname, 'storage.htm
 app.get("/addForm", (req, res) => res.sendFile(path.join(__dirname, 'addForm.html')));
 // API paths
 app.get("/api/chemicals", (req, res) => res.json(chemicals));
+
+// Post to api/chemicals
+app.post("/api/chemicals", (req, res) => {
+    const chemical = req.body;
+
+    count++;
+    chemical.id = count;
+    console.log(chemical);
+
+    chemicals.push(chemical);
+    res.send(chemicals)
+});
 
 // Listen on port
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
