45ada1b805f5ae18915674a970fe4551f655079d
Nathan Peek <nmp14fsu@gmail.com>
2021-03-10 02:09:25 -0500

    Add routes and sequelize. Move route requests to different files

--- server.js
+++ server.js
@@ -1,37 +1,19 @@
 const express = require("express");
-const path = require("path");
+const sequelize = require("./config/connect.js");
+const routes = require("./routes")
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const chemicals = [];
-let count = 0;
-
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+// Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Paths
 app.use("/assets", express.static('assets'));
 
-// HTML paths
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-app.get('/storage', (req, res) => res.sendFile(path.join(__dirname, 'storage.html')));
-app.get("/addForm", (req, res) => res.sendFile(path.join(__dirname, 'addForm.html')));
-// API paths
-app.get("/api/chemicals", (req, res) => res.json(chemicals));
+// Routes
+app.use(routes);
 
-// Post to api/chemicals
-app.post("/api/chemicals", (req, res) => {
-    const chemical = req.body;
-
-    count++;
-    chemical.id = count;
-    console.log(chemical);
-
-    chemicals.push(chemical);
-    res.send(chemicals)
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
 });
-
-// Listen on port
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
