52bd8870ec9e945234a6795eab61eb6bb03f7693
Nathan Peek <nmp14fsu@gmail.com>
2021-03-12 15:24:32 -0500

    Create findAll route

--- routes/api/chemicals.js
+++ routes/api/chemicals.js
@@ -1,7 +1,18 @@
 const router = require("express").Router();
+const Chemical = require("../../models/Chemicals");
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+    try {
+        const chemicalResult = await Chemical.findAll();
 
+        if (!chemicalResult) {
+            res.status(404).json("Not Found");
+        }
+
+        res.status(200).json(chemicalResult);
+    } catch (e) {
+        res.status(500).json(e);
+    }
 });
 
 module.exports = router;
