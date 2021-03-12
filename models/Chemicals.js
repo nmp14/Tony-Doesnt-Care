1d6c5c713dbce67c6dca38b2ecebdaa146808081
Nathan Peek <nmp14fsu@gmail.com>
2021-03-12 03:49:22 -0500

    Create chemicals class

---
+++ models/Chemicals.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connect");
+
+class Chemical extends Model { }
+
+Chemical.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        location: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        distributor: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        serialCode: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    }, {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "chemical"
+}
+);
+
+module.exports = Chemical;
