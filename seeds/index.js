b457ad7bf352ff524a700779f0a35f8cee9bfe8c
Nathan Peek <nmp14fsu@gmail.com>
2021-03-13 17:12:53 -0500

    Create seed data for Chemicals table

--- seeds/index.js
+++ seeds/index.js
@@ -1,11 +1,18 @@
 const sequelize = require('../config/connect');
 const Member = require("../models/Members");
+const Chemical = require("../models/Chemicals");
 const memberData = require('./members-seed.json');
+const chemicalData = require("./chemical-seed.json");
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: false });
 
     await Member.bulkCreate(memberData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Chemical.bulkCreate(chemicalData, {
         individualHooks: true,
         returning: true,
     });
