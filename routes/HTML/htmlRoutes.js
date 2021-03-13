3cd7f2c4046d12340eb2d7224600bc780adddb45
Nathan Peek <nmp14fsu@gmail.com>
2021-03-12 19:18:37 -0500

    Add route for member html

--- routes/HTML/htmlRoutes.js
+++ routes/HTML/htmlRoutes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const path = require("path");
 
-router.get("/", (req, res) => res.sendFile(path.join(__dirname, "../../index.html")));
-router.get("/storage", (req, res) => res.sendFile(path.join(__dirname, "../../storage.html")));
-router.get("/addForm", (req, res) => res.sendFile(path.join(__dirname, "../../addForm.html")));
+router.get("/", (req, res) => res.sendFile(path.join(__dirname, "../../HTML/index.html")));
+router.get("/storage", (req, res) => res.sendFile(path.join(__dirname, "../../HTML/storage.html")));
+router.get("/addForm", (req, res) => res.sendFile(path.join(__dirname, "../../HTML/addForm.html")));
+router.get("/members", (req, res) => res.sendFile(path.join(__dirname, "../../HTML/members.html")));
 
 module.exports = router;
