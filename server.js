40deb3afef3370651cd73ebf74cf9e6e7cd94d27
Nathan Peek <nmp14fsu@gmail.com>
2021-03-13 15:05:20 -0500

    Swap to handlebars and controllers instead of routes

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const sequelize = require("./config/connect.js");
-const routes = require("./routes")
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+const hbs = exphbs.create({});
 
 // Middleware
 app.use(express.json());
@@ -12,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/assets", express.static('assets'));
 
 // Routes
-app.use(routes);
+app.use(require("./controllers"));
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
