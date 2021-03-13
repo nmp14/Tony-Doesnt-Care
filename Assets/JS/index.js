a16558f9f9e8dc2d107ec986be8f11047efd9f4b
Nathan Peek <nmp14fsu@gmail.com>
2021-03-12 20:15:52 -0500

    save point

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -2,6 +2,28 @@ const bossCard = document.getElementById("bossCard");
 const memberCards = document.getElementById("memberCards");
 const graduatedCards = document.getElementById("graduatedCards");
 
+// Test objects before db connection.
+const members = [{
+    name: "Dr. Albert Stiegman",
+    email: "stiegman@fsu.edu",
+    role: "boss"
+}, {
+    id: 2,
+    name: "Tony",
+    email: "tony@gmail.com",
+    role: "student"
+}, {
+    id: 3,
+    name: "cera",
+    email: "cera@gmail.com",
+    role: "student"
+}, {
+    id: 4,
+    name: "su",
+    email: "su@gmail.com",
+    role: "graduate"
+}];
+
 function docReady(fn) {
     // see if DOM is already available
     if (document.readyState === "complete" || document.readyState === "interactive") {
@@ -19,8 +41,14 @@ docReady(function () {
             renderCards();
         }
     }
-
+    // Loop through members and call function to create HTML
     const renderCards = () => {
+        for (const member of members) {
+            generateCard(member);
+        }
+    }
+    // Create html for each member card
+    const generateCard = member => {
 
     }
 
