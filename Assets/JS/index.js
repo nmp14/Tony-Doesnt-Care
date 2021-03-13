6a5d8dd03068145ab4ee85372abc38413d350dbc
Nathan Peek <nmp14fsu@gmail.com>
2021-03-12 19:07:20 -0500

    Add doc ready function

---
+++ Assets/JS/index.js
@@ -0,0 +1,16 @@
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+testArr = ["name1", "name2", "name3"];
+
+// DOM is loaded and ready for manipulation here
+docReady(function () {
+
+});
