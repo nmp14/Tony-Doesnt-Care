1db529dc120cc6b8db7dfa7b51704c284ea30398
Nathan Peek <nmp14fsu@gmail.com>
2021-03-20 02:46:35 -0400

    Create logout function

---
+++ Assets/JS/logout.js
@@ -0,0 +1,16 @@
+const logout = document.getElementById("logout");
+
+const logoutFunction = async () => {
+    const response = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Failed to log out.');
+    }
+};
+
+logout.addEventListener('click', logoutFunction);
