5e72a66ed4182767f25795565cf80fa9918f2abc
Nathan Peek <nmp14fsu@gmail.com>
2021-03-20 02:48:29 -0400

    Add userRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const chemicals = require("./chemicals");
 const members = require("./members");
+const userRoutes = require("./userRoutes");
 
 router.use("/chemicals", chemicals);
 router.use("/members", members);
+router.use("/users", userRoutes);
 
 module.exports = router;
