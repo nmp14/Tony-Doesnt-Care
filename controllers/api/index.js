742bc3f32a1850692bd9d6a223420b14b9aa9234
Nathan Peek <nmp14fsu@gmail.com>
2021-04-20 02:05:53 -0400

    Remove unnecessary route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
 const chemicals = require("./chemicals");
-const users = require("./users");
 const userRoutes = require("./userRoutes");
 
 router.use("/chemicals", chemicals);
-router.use("/members", users);
 router.use("/users", userRoutes);
 
 module.exports = router;
